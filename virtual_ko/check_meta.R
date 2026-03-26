2778ed2513bcb2806f97bfe60a3a7ad6a99b968d
QianhuiWan <wanqianhui@outlook.com>
2026-03-26 11:38:20 -0700

    check single cell meta

---
+++ virtual_ko/check_meta.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+
+colonCan_meta <- read_tsv("/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/qianhui/projects/kzfp/colon_singleCell/SCP1162/metadata/metatable_v3_fix_v3.tsv") %>% slice(-1)
+
+
